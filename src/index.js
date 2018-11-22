2a4a472115219b3f632930b51e7688570695166b
Raphael Hättich <raphael@haettich.ch>
2018-11-22 16:13:56 +0100

    style: renamed variable

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-module.exports = (attribute, type, throwError = true) => {
-    if (typeof attribute === type || (typeof type === 'function' && attribute instanceof type)) {
+module.exports = (value, type, throwError = true) => {
+    if (typeof value === type || (typeof type === 'function' && value instanceof type)) {
         return true;
     }
     if(throwError) {
-        throw new Error(`${attribute} is not of type: ${type.toString()}`);
+        throw new Error(`${value} is not of type: ${type.toString()}`);
     }
     return false;
 };
