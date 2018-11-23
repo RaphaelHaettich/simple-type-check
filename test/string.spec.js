20f7eb3ce9f64b6ddf8453cac3b5e30d7cadcdb7
Raphael Hättich <raphael@haettich.ch>
2018-11-23 09:15:20 +0100

    chore: linting

--- test/string.spec.js
+++ test/string.spec.js
@@ -1,4 +1,5 @@
-const expect = require('chai').expect;
+/* eslint-disable no-undef */
+const { expect } = require('chai');
 const simpleTypeCheck = require('../src');
 
 describe('check type string', () => {
