5f1f1e94c3cb70acc725598058bcbb85cdf1f29a
Raphael Hättich <raphael@haettich.ch>
2018-11-23 09:05:10 +0100

    chore: linting

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 module.exports = (value, type, throwError = true) => {
-    if (typeof value === type || (typeof type === 'function' && value instanceof type)) {
-        return true;
-    }
-    if(throwError) {
-        throw new Error(`${value} is not of type: ${type.toString()}`);
-    }
-    return false;
+  if (typeof value === type || (typeof type === 'function' && value instanceof type)) {
+    return true;
+  }
+  if (throwError) {
+    throw new Error(`${value} is not of type: ${type.toString()}`);
+  }
+  return false;
 };
