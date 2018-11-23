ab656093c7f193c96eaf3353de29e8e8ce3a8e1d
Raphael Hättich <raphael@haettich.ch>
2018-11-23 09:21:50 +0100

    chore: linting

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,7 @@
 module.exports = {
-    "extends": "airbnb-base"
+    "extends": "airbnb-base",
+    "rules": {
+        'valid-typeof': ['error', { requireStringLiterals: false }],
+        "indent": ["error", 4]
+    }
 };
--- test/string.spec.js
+++ test/string.spec.js
@@ -3,43 +3,43 @@ const { expect } = require('chai');
 const simpleTypeCheck = require('../src');
 
 describe('check type string', () => {
-  it('string is detected as string', () => {
-    const expectedResult = true;
-    const value = 'my string';
-    const type = 'string';
-    const receivedResult = simpleTypeCheck(value, type, false);
-    expect(receivedResult).to.be.equal(expectedResult);
-  });
+    it('string is detected as string', () => {
+        const expectedResult = true;
+        const value = 'my string';
+        const type = 'string';
+        const receivedResult = simpleTypeCheck(value, type, false);
+        expect(receivedResult).to.be.equal(expectedResult);
+    });
 
-  it('number is detected as not a string', () => {
-    const expectedResult = false;
-    const value = 123;
-    const type = 'string';
-    const receivedResult = simpleTypeCheck(value, type, false);
-    expect(receivedResult).to.be.equal(expectedResult);
-  });
+    it('number is detected as not a string', () => {
+        const expectedResult = false;
+        const value = 123;
+        const type = 'string';
+        const receivedResult = simpleTypeCheck(value, type, false);
+        expect(receivedResult).to.be.equal(expectedResult);
+    });
 
-  it('function is detected as not a string', () => {
-    const expectedResult = false;
-    const value = () => { };
-    const type = 'string';
-    const receivedResult = simpleTypeCheck(value, type, false);
-    expect(receivedResult).to.be.equal(expectedResult);
-  });
+    it('function is detected as not a string', () => {
+        const expectedResult = false;
+        const value = () => { };
+        const type = 'string';
+        const receivedResult = simpleTypeCheck(value, type, false);
+        expect(receivedResult).to.be.equal(expectedResult);
+    });
 
-  it('bool is detected as not a string', () => {
-    const expectedResult = false;
-    const value = true;
-    const type = 'string';
-    const receivedResult = simpleTypeCheck(value, type, false);
-    expect(receivedResult).to.be.equal(expectedResult);
-  });
+    it('bool is detected as not a string', () => {
+        const expectedResult = false;
+        const value = true;
+        const type = 'string';
+        const receivedResult = simpleTypeCheck(value, type, false);
+        expect(receivedResult).to.be.equal(expectedResult);
+    });
 
-  it('object is detected as not a string', () => {
-    const expectedResult = false;
-    const value = {};
-    const type = 'string';
-    const receivedResult = simpleTypeCheck(value, type, false);
-    expect(receivedResult).to.be.equal(expectedResult);
-  });
+    it('object is detected as not a string', () => {
+        const expectedResult = false;
+        const value = {};
+        const type = 'string';
+        const receivedResult = simpleTypeCheck(value, type, false);
+        expect(receivedResult).to.be.equal(expectedResult);
+    });
 });
